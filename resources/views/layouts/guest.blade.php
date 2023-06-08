283c020e0fa8d7963781ae39b8d556534e0b0527
James Latten <james.latten@phntm.com>
2023-06-08 17:55:09 -0400

    Link fixes

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,7 +9,7 @@
     <meta name="keywords" content="{{ $keywords ?? 'Lnkrr, link, shortener, free, custom, track, links' }}">
 
     <!-- Favicon -->
-    <link rel="icon" href="{{ asset('imgs/lnkrr_black_circle.png') }}" type="image/png">
+    <link rel="icon" href="<?php echo env('APP_URL'); ?>/imgs/lnkrr_black_circle.png" type="image/png">
 
     <!-- Base URL -->
     <meta name="base-url" content="<?php echo env('APP_URL'); ?>">
@@ -18,9 +18,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/fontawesome.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/brands.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/solid.css') }}">
+    <link rel="stylesheet" href="<?php echo env('APP_URL'); ?>/assets/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="<?php echo env('APP_URL'); ?>/assets/fontawesome/css/brands.css">
+    <link rel="stylesheet" href="<?php echo env('APP_URL'); ?>/assets/fontawesome/css/solid.css">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -28,8 +28,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Geologica:wght@100;200;300;400;500;600;700;800;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 
     <!-- Bootstrap -->
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    <link href="<?php echo env('APP_URL'); ?>/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo env('APP_URL'); ?>/css/styles.css" rel="stylesheet">
 </head>
 <body>
 <div class="website">
@@ -51,12 +51,12 @@
 @endenv
 
 <!-- jQuery -->
-<script src="{{ asset('js/jquery.js') }}"></script>
+<script src="<?php echo env('APP_URL'); ?>/js/jquery.js"></script>
 
 <!-- Bootstrap -->
-<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+<script src="<?php echo env('APP_URL'); ?>/js/bootstrap.bundle.min.js"></script>
 
 <!-- Custom Theme Scripts -->
-<script src="{{ asset('js/scripts.js') }}"></script>
+<script src="<?php echo env('APP_URL'); ?>/js/scripts.js"></script>
 </body>
 </html>

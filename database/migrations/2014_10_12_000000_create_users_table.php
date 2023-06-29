084a9524b028a774945770ef86cf159930fa51f5
James Latten <james.latten@phntm.com>
2023-06-29 09:02:48 -0400

    New stuff

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateUsersTable extends Migration
 {
@@ -20,14 +21,15 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->default('')->nullable();
-            $table->binary('profile_pic')->nullable();
             $table->string('font_family')->default('')->nullable();
             $table->string('bio')->default('')->nullable();
-            $table->binary('background_pic')->nullable();
             $table->string('bio_links')->default('')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::statement("ALTER TABLE users ADD background_pic LONGBLOB NULL");
+        DB::statement("ALTER TABLE users ADD profile_pic LONGBLOB NULL");
     }
 
     /**
---
+++ database/migrations/2023_06_28_173443_add_username_column_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUsernameColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Add username column to users table
+            $table->string('username')->unique()->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
